6e20c82f197ac0c9c8266f2e8839f115cdfa60cc
DiasALianZ <xam807046347@qq.com>
2021-04-22 17:46:51 +0800

    change on chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -59,7 +59,7 @@ def hello_command(update: Update, context: CallbackContext) -> None:
 
 def diet_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /diet is issued."""
-    update.message.reply_text('''Vegetable calorimeter
+    update.message.reply_text('Vegetable calorimeter
     Name (100g) calories (kcal)
     Green pepper 24
     Mushroom 24
@@ -73,25 +73,25 @@ def diet_command(update: Update, context: CallbackContext) -> None:
     Cabbage 21
     Cucumber 13
     Balsam pear 20
-    Mung bean 332''')
+    Mung bean 332')
 
 def weight_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /weight is issued."""
-    update.message.reply_text('''Track and field: 450 calories per half hour. It can exercise the whole body.
+    update.message.reply_text('Track and field: 450 calories per half hour. It can exercise the whole body.
     Basketball: 250 calories per half hour. It can enhance flexibility and strengthen cardiopulmonary function.
     Bicycle: it consumes 330 calories every half hour. It's good for heart, lung and legs.
     Riding: 175 calories per half hour. Good for thigh and will training.
     Water skiing: 240 calories per half hour. It has a good exercise effect on the whole body, limbs muscles and balance ability.
     Golf: 125 calories per half hour. Its training effect comes from the long journey and batting action. If we can persevere, it is very beneficial to keep the lines beautiful.
     Jogging: 300 calories per half hour. Good for heart, lung and blood circulation. The longer you run, the more calories you burn.
-    Walking: 75 calories per half hour. It can improve blood circulation, move joints and help to lose weight.''')
+    Walking: 75 calories per half hour. It can improve blood circulation, move joints and help to lose weight.')
 
 def gym_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /gym is issued."""
-    update.message.reply_text('''Methods of reducing fat: strength training plus aerobic
+    update.message.reply_text('Methods of reducing fat: strength training plus aerobic
     Many people come to the gym only to run, can you reduce fat? Answer: of course. But this body will not be good, there will be muscle loss in the process of fat reduction, and the maintenance of the body is the need for muscle.
     Aerobic exercise is recommended to be maintained for more than 30 minutes at low and medium intensity. It's not that fat is consumed after 30 minutes, but that the proportion of energy supplied by fat increases with time. What is medium low intensity? There is no fixed standard for this, but it is generally maintained at 55% - 70% of the maximum heart rate during exercise.
-    Maximum heart rate formula = 220 - one year old''')
+    Maximum heart rate formula = 220 - one year old')
 
 def add(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /add is issued."""
